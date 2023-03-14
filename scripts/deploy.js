f3a1b74108332693f8a5b627ca9f4c8c31e8ff59
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2023-03-14 19:14:04 +0000

    Write the address of the token to a file

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,9 +1,12 @@
 const { ethers } = require('hardhat');
+const fs = require('fs');
 
 async function main() {
-	const transactionCount = await owner.getTransactionCount();
 	// Get owner address
 	const [owner] = await ethers.getSigners();
+
+	const transactionCount = await owner.getTransactionCount();
+
 	// gets the address of the token before it is deployed
 	const futureAddress = ethers.utils.getContractAddress({
 		from: owner.address,
@@ -11,7 +14,11 @@ async function main() {
 	});
 
 	const MyGovernor = await ethers.getContractFactory('MyGovernor');
-	const governor = await MyGovernor.deploy(futureAddress);
+	const governor = await MyGovernor.deploy(
+		futureAddress,
+		25 /* 5 minutes */,
+		300 /* 1 hour */
+	);
 
 	const MyToken = await ethers.getContractFactory('MyToken');
 	const token = await MyToken.deploy(governor.address);
@@ -20,6 +27,14 @@ async function main() {
 		`Governor deployed to ${governor.address}`,
 		`Token deployed to ${token.address}`
 	);
+
+	fs.writeFileSync(
+		// writes the address of the token to a file
+		'./scripts/_contracts.js',
+		`module.exports = {\n    governorAddress: "${governor.address}",\n    tokenAddress: "${token.address}"\n};`
+	);
+
+	await token.delegate(owner.address);
 }
 
 main().catch((error) => {
