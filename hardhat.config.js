ab9b36fc483694d32d2290d34a4132c2058d4868
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2023-03-14 19:20:56 +0000

    Added hardhat config for the goerli network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,20 @@
 require('@nomicfoundation/hardhat-toolbox');
 require('dotenv').config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
 	solidity: '0.8.17',
+	networks: {
+		hardhat: {
+			chainId: 1337,
+		},
+		goerli: {
+			url: process.env.ALCHEMY_GOERLI_URL,
+			accounts: [process.env.GOERLI_PRIVATE_KEY],
+		},
+	},
+	etherscan: {
+		apiKey: {
+			goerli: ETHERSCAN_KEY,
+		},
+	},
 };
